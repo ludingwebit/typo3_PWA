1b0b1f680a85d35eefcfdee35b1e966d3ad2bd7c
Paul <luding@webit.de>
2019-01-12 10:03:45 +0100

    -commit

---
+++ test_project/web/typo3conf/ext/acme_reservation/Classes/Domain/Repository/ReservierungRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WebitDe\AcmeReservation\Domain\Repository;
+
+/***
+ *
+ * This file is part of the "Acme Reservation" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018
+ *
+ ***/
+
+/**
+ * The repository for Reservierungs
+ */
+class ReservierungRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+}
